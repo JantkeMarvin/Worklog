286a89cacf78a52559e188a0880c8ef6ced5c799
JantkeMarvin <JantkeMarvin@gmx.de>
2026-01-13 19:03:55 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = "worklog-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE);
+    await cache.addAll(ASSETS);
+    self.skipWaiting();
+  })());
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => (k !== CACHE) ? caches.delete(k) : Promise.resolve()));
+    self.clients.claim();
+  })());
+});
+
+// Cache-first (für Offline-Start), im Hintergrund aktualisieren
+self.addEventListener("fetch", (event) => {
+  event.respondWith((async () => {
+    const cached = await caches.match(event.request);
+    const fetchPromise = fetch(event.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return res;
+    }).catch(() => cached);
+
+    return cached || fetchPromise;
+  })());
+});
